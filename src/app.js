6cafdc0be2317c65c966943715886fcac8321f81
Ajay David <antonio.g.david@gmail.com>
2017-05-23 13:17:27 -0400

    conditionally show loginForm

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common'; //referencing directory defaults to index.js
+import { Header, Button } from './components/common'; //referencing directory defaults to index.js
 import LoginForm from './components/LoginForm';
 
 
 class App extends Component {
+  state = { loggedIn: false };
+
   componentWillMount() {
     firebase.initializeApp({
         apiKey: 'AIzaSyA1cPeM7meOykxdAGEIpVBsZh-dNd2zIgc',
@@ -15,13 +17,34 @@ class App extends Component {
         storageBucket: 'authentication-42301.appspot.com',
         messagingSenderId: '663692018762'
       });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      // called when sign-in/sign-out, determines presence of user
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    if (this.state.loggedIn) {
+      return (
+        <Button>
+          Log Out
+        </Button>
+      );
+    }
+
+    return <LoginForm />;
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -12,14 +12,29 @@ class LoginForm extends Component {
     this.setState({ error: '', loading: true });
 
     firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(this.onLoginSuccess.bind(this))
       .catch(() => {
         // if fail sign-in, enter this function
         firebase.auth().createUserWithEmailAndPassword(email, password)
-          .catch(() => {
-            // if fail sign-up, enter this function
-            this.setState({ error: 'Authentication Failed.' });
-          });
+          .then(this.onLoginSuccess.bind(this))
+          .catch(this.onLoginFail.bind(this));
       });
+  }
+
+  onLoginFail() {
+    this.setState({
+      error: 'Authentication Failed.',
+      loading: false
+    });
+  }
+
+  onLoginSuccess() {
+    this.setState({
+      email: '',
+      password: '',
+      loading: false,
+      error: ''
+    });
   }
 
   renderButton() {
